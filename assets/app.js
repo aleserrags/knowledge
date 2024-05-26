61dcb055134c2b1986f4aadf078a2b61ceec3702
aleserrags <aleserra.gs@gmail.com>
2024-05-26 01:39:30 -0400

    Configurado bootstrap no projeto

--- assets/app.js
+++ assets/app.js
@@ -8,3 +8,6 @@ import './bootstrap.js';
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/global.css';
+import 'bootstrap'
+import '@popperjs/core'
