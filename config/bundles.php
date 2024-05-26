2e73d634da69ed12767351eebe694c3244fe2df6
aleserrags <aleserra.gs@gmail.com>
2024-05-26 01:39:57 -0400

    Add makeBundle

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
